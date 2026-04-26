a543a63273a3d7483a6b08c99e97da8853d1642f
gringono <kobe135@onlinehome.de>
2026-04-26 11:37:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'produktionsrechner-v1';
+const CACHE = 'produktionsrechner-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
